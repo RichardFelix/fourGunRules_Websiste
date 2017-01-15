3006f48640b366a25ca2e7deab5d73b324a88b21
Richard Felix <rfelixmail@gmail.com>
2017-01-15 18:06:42 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path'),
+      extractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: 'build/'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+              use: ["style-loader", "css-loader", "sass-loader"],
+              test: /\.scss$/
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { limit: 40000 }
+                    },
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new extractTextPlugin('style.css')
+    ]
+};
+
+module.exports = config;
